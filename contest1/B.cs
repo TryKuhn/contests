15d07d41b2d7c24b6219edba37f43cf7d6c08b9c
Egor <65305629+TryKuhn@users.noreply.github.com>
2022-11-15 00:30:34 +0300

    Create B.cs

---
+++ contest1/B.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Globalization;
+using System.Net.Mime;
+
+namespace Solve
+{
+    internal class Program
+    {
+        public static int ReadInt()
+        {
+            string s = Console.ReadLine();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!Char.IsDigit(s[i]))
+                {
+                    Console.WriteLine("Incorrect input");
+                    Environment.Exit(0);
+                }
+            }
+            return Convert.ToInt32(s);
+        }
+        
+        public static int Main(string[] args)
+        {
+            
+            int a = ReadInt();
+            int b = ReadInt();
+            Console.WriteLine(a - b);
+            return 0;
+        }
+    }
+}
