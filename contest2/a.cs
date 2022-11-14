c9ab449f97698a95a848e2990019262e250847a7
Egor <65305629+TryKuhn@users.noreply.github.com>
2022-11-15 00:34:14 +0300

    Create a.cs

---
+++ contest2/a.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Solve
+{
+    internal class Program
+    {
+        const int N = (int)1e5;
+        public static void Main(string[] args)
+        {
+            string s;
+            s = Console.ReadLine();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
